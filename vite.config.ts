f31764eb55a3cd119a2315b55a2400ee15c52bd7
Polo1120 <edgardopolo37@gmail.com>
2026-02-17 17:52:40 -0500

    new change

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [
-      'm6rgye-ip-186-118-197-28.tunnelmole.net'
-    ]
   },
 })
